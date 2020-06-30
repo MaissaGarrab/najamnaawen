9a64f5baea988e9cedf38e1763759f2e22203522
MaissaGarrab <60829314+MaissaGarrab@users.noreply.github.com>
2020-06-30 11:40:52 +0100

    Syntax error fixed

--- src/app/Model/cv.ts
+++ src/app/Model/cv.ts
@@ -5,10 +5,10 @@ export class Cv {
   location: string;
   email: string;
   phoneNumber: number;
-  skills: string[];
-  languages: string[];
-  workExperience: string[];
-  education: string[];
+  skills: String[];
+  languages: String[];
+  workExperience: String[];
+  education: String[];
   constructor(name: string, firstName: string, job: string, location: string, email: string, phoneNumber: number,
               skills: string[], languages: string[], workExperience: string[], education: string[]) {
     this.name = name;
